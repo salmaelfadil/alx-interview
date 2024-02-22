b1a33e9931e6637397d228e395455508f3eac788
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-02-22 21:40:16 +0000

    .

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+minimum operations"""
+
+
+def minOperations(n):
+    """minimum possible operations"""
+    if n <= 1:
+        return n
+
+    operations = 0
+    current_char = 1
+
+    clipboard = 0
+
+    while current_char < n:
+        if n % current_char == 0:
+            clipboard = current_char
+            operations += 2
+            current_char *= 2
+        else:
+            operations += 1
+            current_char += clipboard
+
+    return operations
