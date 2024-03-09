bef65b20fd7b858468ecc9ff7e0610070ad825b9
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-09 18:43:48 +0000

    .

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -7,30 +7,50 @@ validUTF8 = __import__('0-validate_utf8').validUTF8
 
 data = [65]
 print(validUTF8(data))
+print ("should be true")
+print("######")
 
 data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
 print(validUTF8(data))
+print ("should be true")
+print("######")
 
 data = [229, 65, 127, 256]
 print(validUTF8(data))
+print ("should be false")
+print("######")
 
 data =[467, 133, 108]
 print(validUTF8(data))
+print ("should be false")
+print("######")
 
 data = [240, 188, 128, 167]
 print(validUTF8(data))
+print ("should be true")
+print("######")
 
 data = [235, 140]
 print(validUTF8(data))
+print ("should be false")
+print("######")
 
 data = [345, 467]
 print(validUTF8(data))
+print ("should be false")
+print("######")
 
 data =  [250, 145, 145, 145, 145]
 print(validUTF8(data))
+print ("should be true")
+print("######")
 
 data = [0, 0, 0, 0, 0, 0]
 print(validUTF8(data))
+print ("should be true")
+print("######")
 
 data = []
 print(validUTF8(data))
+print ("should be true")
+print("######")
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """UTF-8 Encoding Module"""
-
+import codecs
 
 def validUTF8(data):
     """validate if data is utf-8 encoded"""
-    for c in data:
-        try:
-            bytes([c]).decode('utf-8')
-        except:
-            return False
-    return True
+    try:
+        filtered_data = [byte for byte in data if 0 <= byte <= 255]
+        codecs.decode(bytes(filtered_data), 'utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
