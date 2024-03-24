a4677cbbecd93f71bfbe3bb9b2ab6b65a3fe650f
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-24 15:38:11 -0700

    .

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,49 +1,48 @@
 #!/usr/bin/node
 const request = require('request');
 
-let url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-function getData(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-            try {
-                const characters = JSON.parse(body).characters;
-                resolve(characters);
-            } catch (parseError) {
-                reject(new Error("Error parsing film data"));
-            }
-        });
+function getData (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      try {
+        const characters = JSON.parse(body).characters;
+        resolve(characters);
+      } catch (parseError) {
+        reject(new Error('Error parsing film data'));
+      }
     });
+  });
 }
 
-function getChars(characters) {
-    const promises = characters.map(url => {
-        return new Promise((resolve, reject) => {
-            request(url, (error, response, body) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                try {
-                    const character = JSON.parse(body);
-                    resolve(character.name);
-                } catch (parseError) {
-                    reject(new Error("Error parsing character data"));
-                }
-            });
-        });
+function getChars (characters) {
+  const promises = characters.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        try {
+          const character = JSON.parse(body);
+          resolve(character.name);
+        } catch (parseError) {
+          reject(new Error('Error parsing character data'));
+        }
+      });
     });
-    Promise.all(promises)
-        .then(names => {
-            names.forEach(name => console.log(name));
-        })
-        .catch(error => console.error(error));
+  });
+  Promise.all(promises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => console.error(error));
 }
 getData(url)
-    .then(getChars)
-    .catch(error => console.error(error));
-
+  .then(getChars)
+  .catch(error => console.error(error));
